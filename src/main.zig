574889b28cd9f8c9fe53abb0341ed4f3e006563b
Daniel A.C. Martin <github@daniel-martin.co.uk>
2023-08-12 19:13:32 +0100

    Better print of boot_info

--- src/main.zig
+++ src/main.zig
@@ -27,30 +27,18 @@ export fn main() void {
         \\Boot info:
         \\
     );
+    seL4.debugPrintBootInfo(boot_info);
 
-    seL4.debugPutString("extraLen: ");
-    seL4.debugPutString(&word2string(boot_info.*.extraLen));
-    seL4.debugPutString(";\n");
-
-    seL4.debugPutString("nodeID: ");
-    seL4.debugPutString(&word2string(boot_info.*.nodeID));
-    seL4.debugPutString(";\n");
-
-    seL4.debugPutString("numNodes: ");
-    seL4.debugPutString(&word2string(boot_info.*.numNodes));
-    seL4.debugPutString(";\n");
-
-    seL4.debugPutString("numIOPTLevels: ");
-    seL4.debugPutString(&word2string(boot_info.*.numIOPTLevels));
-    seL4.debugPutString(";\n");
-
-    seL4.debugPutString("initThreadCNodeSizeBits: ");
-    seL4.debugPutString(&word2string(boot_info.*.initThreadCNodeSizeBits));
-    seL4.debugPutString(";\n");
-
-    seL4.debugPutString("initThreadDomain: ");
-    seL4.debugPutString(&word2string(boot_info.*.initThreadDomain));
-    seL4.debugPutString(";\n");
-
+    seL4.debugPutString(
+        \\
+        \\Scheduler:
+        \\
+    );
     seL4.debugDumpScheduler();
+
+
+
+
+
+
 }
